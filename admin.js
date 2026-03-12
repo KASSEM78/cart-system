1d5f9da5580176355b4ab6b48572a5d7ff722172
KASSEM78 <kinvthe900@gmail.com>
2026-03-12 20:55:57 +0200

    local storage

---
+++ admin.js
@@ -0,0 +1,41 @@
+let tbody = document.querySelector("tbody");
+
+let phoneName = document.querySelector("#phoneName");
+let phonePrice = document.querySelector("#phonePrice");
+let phoneImg = document.querySelector("#phoneImg");
+let phoneBrand = document.querySelector("#phoneBrand");
+
+let showProducts = () => {
+  tbody.innerHTML = "";
+  products.forEach((el, index) => {
+    tbody.innerHTML += `
+        <tr>
+            <td>${index + 1}</td>
+            <td>${el.name}</td>
+            <td>${el.price}</td>
+            <td>
+                <img height="50" width="50" src="${el.img}"/>
+            </td>
+            <td>
+                <button class="btn btn-danger">Edit</button>
+                <button class="btn btn-primary">Remove</button>
+            </td>
+        </tr>
+        `;
+  });
+};
+
+let addNewProduct = () => {
+  let newProduct = {
+    name: phoneName.value,
+    price: phonePrice.value,
+    brand: phoneBrand.value,
+    img: phoneImg.value,
+  };
+  orignalProducts.push(newProduct);
+  let productJSON = JSON.stringify(orignalProducts)
+  localStorage.setItem('products',productJSON)
+  showProducts();
+};
+
+showProducts();
